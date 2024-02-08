7131cfce8176c7a325e519e1a2449d55d5044974
Alern <alerncheruiyot@gmail.com>
2024-02-09 02:29:41 +0300

    Final test for the creds stored in terminal

--- app/Http/Controllers/IdeasController.php
+++ app/Http/Controllers/IdeasController.php
@@ -10,10 +10,10 @@ class IdeasController extends Controller
 
 public function myIdeas(){
     $idea = new Idea([
-    'name' => "fresh idea",
+    'name' => "stored creds ideas",
     'qty' => 69,
     'price' => 47.0,
-    'description' => 'This is an array input format to the db using the model'
+    'description' => 'This is a test for the creds stored automatically'
     ]);
     $idea -> save();
 
