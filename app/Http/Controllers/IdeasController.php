16b808682c7e8bd47d00b42c02ee84a7a792e5c3
Alern <alerncheruiyot@gmail.com>
2024-02-09 01:48:00 +0300

    Array configurations for idea model to input dat to the DB

--- app/Http/Controllers/IdeasController.php
+++ app/Http/Controllers/IdeasController.php
@@ -9,11 +9,12 @@ class IdeasController extends Controller
 {
 
 public function myIdeas(){
-    $idea = new Idea();
-    $idea -> name = 'my first idea';
-    $idea -> qty = 17;
-    $idea -> price = 10.3;
-    $idea -> description ='JUst trying to understand laravel';
+    $idea = new Idea([
+    'name' => "fresh idea",
+    'qty' => 69,
+    'price' => 47.0,
+    'description' => 'This is an array input format to the db using the model'
+    ]);
     $idea -> save();
 
     return view('ideas.ideas');
